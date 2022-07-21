1b9b7cff1dcccf7a2afcaaefe4f6dfb30272a769
Grace Wang <graceannwang@gmail.com>
2022-07-21 14:52:03 -0500

    update gen_links

--- gen_links.py
+++ gen_links.py
@@ -16,7 +16,7 @@ num_sites = 0
 with open('meta.txt') as meta_fh:
     num_sites = int(meta_fh.readline())
 
-#
+# scrape links and add to new link file for each site
 for i in range(1, num_sites + 1):
     resp_filename = 'resp' + str(i) + '.txt'
     resp_fp = os.path.join(resps_fp, resp_filename)
